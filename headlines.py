4d9ffa2ae228ba2938b577e6f47e0f53facccacb
James Castro <jcastro61@hotmail.com>
2019-02-07 00:38:17 -0500

    Initial commit

---
+++ headlines.py
@@ -0,0 +1,48 @@
+import feedparser
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+RSS_FEEDS = {
+              'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+              'cnn': 'http://rss.cnn.com/rss/edition.rss',
+              'fox': 'http://feeds.foxnews.com/foxnews/latest',
+              'reuters-science': 'http://feeds.reuters.com/reuters/scienceNews',
+              'reuters-topnews': 'http://feeds.reuters.com/reuters/topNews'
+              }
+
+@app.route("/")
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
+
+
+@app.route("/cnn")
+def cnn():
+    return get_news('cnn')
+
+
+@app.route("/fox")
+def fox():
+    return get_news('fox')
+
+
+@app.route("/reuters")
+def reuters_topnews():
+    return get_news('reuters-topnews')
+
+
+@app.route("/reuters/science")
+def reuters():
+    return get_news('reuters-science')
+
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return render_template("home.html", articles=feed['entries'])
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
